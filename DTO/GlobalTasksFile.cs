ee09f540c617260048af37c492ce7d9addac4233
Oerb <bjoern@oerb.de>
2015-10-23 14:01:07 +0200

    Saperion Com Wraper Project Added

---
+++ DTO/GlobalTasksFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class GlobalTasksFile
+    {
+        // Just for VisualStudio TaskManager Use to Add Global Tasks here
+
+        // TODO: Merge Resources from old DTO
+    }
+}
